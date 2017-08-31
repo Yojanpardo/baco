e89b5faa0556a4aac8970cd9bb01847f3817905a
Yojan Pardo <yojan.pardo@gmail.com>
2017-08-30 19:58:24 -0500

    agrego la ruta en urls.py y espero que funcione a la primera

--- products/views.py
+++ products/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.template import loader
+from django.http import HttpResponse
+from .models import Product
 
 # Create your views here.
+
+def index(request):
+	product = Product.object.order_by('id')
+	template = loader.get_template('home.html')
+	context = {
+		'product':product
+	}
+	return HttpResponse(template.render(context, request))
