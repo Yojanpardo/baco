37636bdba652816401d1fdfd3a15ffb73dfa82c8
Yojan Pardo <yojan.pardo@gmail.com>
2017-08-30 19:50:42 -0500

    agrego carpeta templates, configuro la ruta de las plantillas en settings.py

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+
+@admin.register(Product)
+class AdminProduct(admin.ModelAdmin):
+	list_display = ('name', 'category', 'price')
+	list_filter = ('category')
